c21a642797dc323d14863b7f5575c4e3a615fca1
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-05-19 20:22:28 +0500

    test

---
+++ main.js
@@ -0,0 +1,14 @@
+// Element, Class, Event
+
+
+// Eventlar, attributlarni lko'ramiz
+
+// console.log(window.navigator);
+
+// test.addEventListener('change', event => console.log(event.target.value))
+
+// play, stop, pause
+
+// inputElement.addEventListener('select', event => {
+//     console.log(event);
+// })
